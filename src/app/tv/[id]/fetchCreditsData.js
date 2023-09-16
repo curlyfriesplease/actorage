8f743446484a008fd64cd994dd9f31610419084d
Jon Mullingtons <jmulhern85@gmail.com>
2023-09-16 11:34:35 +0100

    Getting there

--- src/app/tv/[id]/fetchCreditsData.js
+++ src/app/tv/[id]/fetchCreditsData.js
@@ -2,10 +2,10 @@ import { checkEnvironment } from '../../functions/checkEnv';
 
 export async function fetchCreditsData(value) {
   if (value.length) {
-    console.log('fetchCreditsData (movie) value received: ' + value);
+    console.log('fetchCreditsData (tv) value received: ' + value);
     try {
       const response = await fetch(
-        checkEnvironment().concat(`/api/movie?query=${value}/credits`)
+        checkEnvironment().concat(`/api/tv?query=${value}/credits`)
       );
       const data = await response.json();
       return data;

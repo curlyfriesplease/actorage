57461327f89a8740e3ab5f3932229cf1a14f6d14
Jon Mullingtons <jmulhern85@gmail.com>
2023-11-08 18:22:51 +0000

    Adds prod url

--- src/app/functions/checkEnv.js
+++ src/app/functions/checkEnv.js
@@ -1,8 +1,8 @@
 export const checkEnvironment = () => {
   let base_url =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://needtotreplacethislater.com";
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://develop.d1rjna21ytrvho.amplifyapp.com/';
 
   return base_url;
 };

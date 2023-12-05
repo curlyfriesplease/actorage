dc1cc750c0d3f762653d7e7eaa135faf0bb7f846
curlyfriesplease <jmulhern85@gmail.com>
2023-12-05 22:10:15 +0000

    Framer is fun

--- components/Search/SearchContainer.jsx
+++ components/Search/SearchContainer.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
 import { Suspense, useState } from 'react';
 import SearchBar from './SearchBar';
 import { SearchResultsList } from './SearchResultsList';
@@ -8,13 +10,18 @@ export default function SearchContainer() {
   const [results, setResults] = useState([]);
   console.dir(results);
   return (
-    <div className="h-200 w-full max-w-lg pt-9">
+    <motion.div
+      className="h-200 w-full max-w-lg pt-9"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1.5, type: 'spring', bounce: 0.75 }}
+    >
       <SearchBar setResults={setResults} />
       <Suspense fallback={<div>Loading...</div>}>
         {results && results.results?.length > 0 && (
           <SearchResultsList results={results} />
         )}
       </Suspense>
-    </div>
+    </motion.div>
   );
 }

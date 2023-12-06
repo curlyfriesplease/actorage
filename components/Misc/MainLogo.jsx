d866c84940d6e0438947d46d91105752b0998896
curlyfriesplease <jmulhern85@gmail.com>
2023-12-06 16:05:00 +0000

    Quack noise

--- components/Misc/MainLogo.jsx
+++ components/Misc/MainLogo.jsx
@@ -1,31 +1,44 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import Image from 'next/image';
 
-export const MainLogo = () => (
-  <motion.div
-    id="main-full-size-logo"
-    className="
+export const MainLogo = () => {
+  const [clickCount, setClickCount] = useState(0);
+
+  useEffect(() => {
+    if (clickCount > 7) {
+      const audio = new Audio('/audio/quack.mp3');
+      audio.play();
+    }
+  }, [clickCount]);
+
+  return (
+    <motion.div
+      id="main-full-size-logo"
+      className="
     relative 
     w-2/3
     pb-full
    "
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.7 }}
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1, rotate: 360 }}
-    transition={{ duration: 1.75, type: 'spring', bounce: 0.5 }}
-  >
-    <Image
-      src="/images/HOWTA_png_full_size_compressed.png"
-      alt="navbarlogo"
-      width={200} // replace with your image's width
-      height={200} // replace with your image's height
-      layout="responsive"
-      objectFit="contain"
-      className="cursor-pointer"
-    />
-  </motion.div>
-);
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.7 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, rotate: 360 }}
+      transition={{ duration: 1.75, type: 'spring', bounce: 0.5 }}
+      onClick={() => setClickCount((prevCount) => prevCount + 1)}
+    >
+      <Image
+        src="/images/HOWTA_png_full_size_compressed.png"
+        alt="navbarlogo"
+        width={200} 
+        height={200} 
+        layout="responsive"
+        objectFit="contain"
+        className="cursor-pointer"
+      />
+    </motion.div>
+  );
+};

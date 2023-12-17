bafdcff316d480f49deceb56ced6ea1d50f0daa0
curlyfriesplease <jmulhern85@gmail.com>
2023-12-17 19:47:34 +0000

    Hides scrollbars

--- components/PopularTitles/PopularTitles.jsx
+++ components/PopularTitles/PopularTitles.jsx
@@ -23,24 +23,24 @@ export const PopularTitles = () => {
     <motion.div
       id="popular-titles"
       className="
-    w-full 
-    max-w-[400px]
-    flex-grow
-    max-h-[40vh]
-    flex-1
-    text-center
-    text-md
-    md:text-xl
-    text-sky-600
-    text-bold
-    overflow-hidden
-    no-scrollbar
-    flex
-    flex-col
-    justify-center
-    items-center
-    rounded-xl
-    "
+        w-full 
+        max-w-[430px]
+        flex-grow
+        max-h-[40vh]
+        flex-1
+        text-center
+        text-md
+        md:text-xl
+        text-sky-600
+        text-bold
+        overflow-hidden
+        no-scrollbar
+        flex
+        flex-col
+        justify-center
+        items-center
+        rounded-xl
+        "
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 2.25, type: 'spring', bounce: 0.75 }}
@@ -77,55 +77,55 @@ export const PopularTitles = () => {
             justify-items-stretch
             w-full
             h-full
-            overflow-auto
+            overflow-hidden
             no-scrollbar
             rounded-xl
           "
           >
             {sixTitleIds.map((title) => (
               <motion.div
-              id='popular-titles-image-container'
+                id="popular-titles-image-container"
                 key={title.id}
                 className="
- 
-              bg-transparent
-              hover:bg-gradient-to-b 
-              from-rose-200 
-              via-indigo-100 
-              to-violet-200
-              rounded-xl 
-              text-center 
-              fade-edges 
-              overflow-hidden
-              cursor-pointer
-              relative
-              flex
-              items-center
-              justify-center
-            "
+                  bg-transparent
+                  hover:bg-gradient-to-b 
+                  from-rose-200 
+                  via-indigo-100 
+                  to-violet-200
+                  rounded-xl 
+                  text-center 
+                  fade-edges 
+                  overflow-hidden
+                  cursor-pointer
+                  relative
+                  flex
+                  items-center
+                  justify-center
+                  "
                 onClick={() => {
                   handleOnClick(title.id);
                 }}
                 whileHover={{ scale: 1.05 }}
               >
-              <div 
-              id='weird-div'
-              className="
-              w-64 
-              lg:w-80
-              h-96 
-              px-1 
-              lg:p-2
-              max-h-full
-              ">
-              <div className="w-full h-full relative">
-                <Image
-                  id="popular-titles-image"
-                  src={`https://image.tmdb.org/t/p/w200${title.img}`}
-                  layout="fill"
-                  objectFit="contain"
-                  alt={title.id}
-                />
+                <div
+                  id="weird-div"
+                  className="
+                    w-64 
+                    lg:w-80
+                    h-96 
+                    px-1 
+                    lg:p-2
+                    max-h-full
+                    "
+                >
+                  <div className="w-full h-full relative">
+                    <Image
+                      id="popular-titles-image"
+                      src={`https://image.tmdb.org/t/p/w200${title.img}`}
+                      layout="fill"
+                      objectFit="contain"
+                      alt={title.id}
+                    />
                   </div>
                 </div>
               </motion.div>

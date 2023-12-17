a3aab0b8a6ca02336e885e8d3004a3cc5e8e6743
Jon Mullingtons <jmulhern85@gmail.com>
2023-12-17 16:25:42 +0000

    Loading spinner size

--- components/Search/SearchResultsList.jsx
+++ components/Search/SearchResultsList.jsx
@@ -53,7 +53,7 @@ export const SearchResultsList = ({ results }) => {
     <Suspense fallback={<div>Loading...</div>}>
       <AnimatePresence>
 
-        {isResultsVisible && (
+        {!isResultsVisible && (
           <motion.div
             id="results-list"
             initial="initial"
@@ -90,7 +90,7 @@ export const SearchResultsList = ({ results }) => {
           </motion.div>
         )}
 
-        {!isResultsVisible && (
+        {isResultsVisible && (
           <div
             className="
             absolute 
@@ -103,8 +103,8 @@ export const SearchResultsList = ({ results }) => {
             <Image
               src="/images/LoadingEclipse.gif"
               alt="Loading"
-              width={60}
-              height={60}
+              width={40}
+              height={40}
             />
           </div>
         )}
